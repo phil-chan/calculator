16d0650bbb91b33c1fdabfffead359420f8429c8
Philip Chan <filnzl@hotmail.com>
2020-06-30 23:47:07 +1200

    added global vars and basic click functions

--- calculator.js
+++ calculator.js
@@ -2,13 +2,21 @@
 
 // init variables
 // ===================================
-// numString ??
+// numString ?? the display
 // numArray to store input numbers
 // display getelement by display
 // a boolean for previous result ??
+var numString = ""; //stores what to display
+var numArray = []; //stores actual numbers for calculation
+var previousResult = false; //maybe when user clicks same operation button
+
+var display = document.getElementById('display');
 
 // function start program listen()
 // has event listner to get pressed button value
+function listen() {
+    document.addEventListener('click', getButtonValue);
+}
 
 // function to getButtonValue()
 // button = event target value
@@ -17,6 +25,14 @@
 // else if CE, clear()
 // else if =, calculate()
 // else, storeNumber(button)
+function getButtonValue() {
+    let buttonVal = event.target.value; //gets the value of clicked thing
+    if(isNaN(buttonVal) || buttonVal === ".") number(buttonVal);
+    else if(buttonVal==="AC") allClear();
+    else if(buttonVal==="CE") clearEntry();
+    else if(buttonVal==="=") calculate();
+    else storeNumber(buttonVal)
+}
 
 // number(button)
 // if button has a dot and numstring also includes a dot, return
